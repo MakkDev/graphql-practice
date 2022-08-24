7b318f84c4f81c1eb3a14e742cf6b7e4fc459a55
MelloWeb3 <amakkieh@gmail.com>
2022-08-24 09:27:22 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+import { useEffect } from 'react';
+
+const GET_QUERY = gql`
+query Query {
+  message
+  year
+  person {
+    name
+    age
+  }
+  people {
+    age
+    name
+  }
+  todos {
+    task
+    id
+    person
+  }
+}
+`
+
 
 function App() {
+
+  const { loading, error, data } = useQuery(GET_QUERY);
+  useEffect(() => {
+    console.log(data)
+  })
+  
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :( </p>;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    data.todos.map(todo => {
+     return <div key={todo.id} className="App">
+        {todo.task}
+      </div>     
+    })
   );
 }
 
